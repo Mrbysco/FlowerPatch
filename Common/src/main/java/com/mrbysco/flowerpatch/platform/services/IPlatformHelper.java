fe76c4b4daa86cfd8ff51f94d59210ec37c9a93a
Mrbysco <Mrbysco@users.noreply.github.com>
2023-05-16 02:16:20 +0200

    Add an option that allows placement of bush-blocks on leaves blocks

--- Common/src/main/java/com/mrbysco/flowerpatch/platform/services/IPlatformHelper.java
+++ Common/src/main/java/com/mrbysco/flowerpatch/platform/services/IPlatformHelper.java
@@ -2,13 +2,18 @@ package com.mrbysco.flowerpatch.platform.services;
 
 public interface IPlatformHelper {
 
-    /**
-     * Returns if patch bonemealing has been enabled in the config
-     */
-    boolean patchBonemealing();
+	/**
+	 * Returns if patch bonemealing has been enabled in the config
+	 */
+	boolean patchBonemealing();
 
-    /**
-     * Returns if flower to patch bonemealing has been enabled in the config
-     */
-    boolean flowerToPatchBonemealing();
+	/**
+	 * Returns if flower to patch bonemealing has been enabled in the config
+	 */
+	boolean flowerToPatchBonemealing();
+
+	/**
+	 * Returns if bush-like blocks should be place-able on leaf blocks
+	 */
+	boolean placeOnLeaves();
 }
