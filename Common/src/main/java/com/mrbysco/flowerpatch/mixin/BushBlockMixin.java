a9222d04b7f62fcb01f94e01a7a7103665ee19ec
Mrbysco <Mrbysco@users.noreply.github.com>
2023-05-19 18:34:35 +0200

    Don't allow worldgen to place on leaves

--- Common/src/main/java/com/mrbysco/flowerpatch/mixin/BushBlockMixin.java
+++ Common/src/main/java/com/mrbysco/flowerpatch/mixin/BushBlockMixin.java
@@ -2,6 +2,7 @@ package com.mrbysco.flowerpatch.mixin;
 
 import com.mrbysco.flowerpatch.platform.Services;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.WorldGenRegion;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.BushBlock;
@@ -17,7 +18,7 @@ public class BushBlockMixin {
 	@Inject(method = "mayPlaceOn(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z",
 			at = @At(value = "HEAD"), cancellable = true)
 	private void flowerpatch_mayPlaceOn(BlockState state, BlockGetter blockGetter, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-		if (Services.PLATFORM.placeOnLeaves() && state.is(BlockTags.LEAVES)) {
+		if (Services.PLATFORM.placeOnLeaves() && !(blockGetter instanceof WorldGenRegion) && state.is(BlockTags.LEAVES)) {
 			cir.setReturnValue(true);
 		}
 	}
