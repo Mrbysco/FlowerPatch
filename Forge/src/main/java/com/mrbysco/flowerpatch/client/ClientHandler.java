ece19a0cca390a39aa0a016ad3ab0ce77f9f53b5
Mrbysco <Mrbysco@users.noreply.github.com>
2023-07-25 19:13:46 +0200

    Fix mushroom patches not rendering correctly

--- Forge/src/main/java/com/mrbysco/flowerpatch/client/ClientHandler.java
+++ Forge/src/main/java/com/mrbysco/flowerpatch/client/ClientHandler.java
@@ -1,6 +1,6 @@
 package com.mrbysco.flowerpatch.client;
 
-import com.mrbysco.flowerpatch.block.FlowerPatchBlock;
+import com.mrbysco.flowerpatch.block.PatchBlock;
 import com.mrbysco.flowerpatch.registration.PatchRegistry;
 import com.mrbysco.flowerpatch.registration.RegistryObject;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
@@ -11,8 +11,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 public class ClientHandler {
 	public static void onClientSetup(final FMLClientSetupEvent event) {
 		for (RegistryObject<Block> registryObject : PatchRegistry.BLOCKS.getEntries()) {
-			if (registryObject.get() instanceof FlowerPatchBlock flowerPatchBlock) {
-				ItemBlockRenderTypes.setRenderLayer(flowerPatchBlock, RenderType.cutout());
+			if (registryObject.get() instanceof PatchBlock) {
+				ItemBlockRenderTypes.setRenderLayer(registryObject.get(), RenderType.cutout());
 			}
 		}
 	}
