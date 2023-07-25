412c381566630620f97bcb78dd691448e83626f9
Mrbysco <Mrbysco@users.noreply.github.com>
2023-07-25 19:13:46 +0200

    Fix mushroom patches not rendering correctly

--- Fabric/src/main/java/com/mrbysco/flowerpatch/FlowerPatchFabricClient.java
+++ Fabric/src/main/java/com/mrbysco/flowerpatch/FlowerPatchFabricClient.java
@@ -1,6 +1,6 @@
 package com.mrbysco.flowerpatch;
 
-import com.mrbysco.flowerpatch.block.FlowerPatchBlock;
+import com.mrbysco.flowerpatch.block.PatchBlock;
 import com.mrbysco.flowerpatch.registration.PatchRegistry;
 import com.mrbysco.flowerpatch.registration.RegistryObject;
 import net.fabricmc.api.ClientModInitializer;
@@ -13,8 +13,8 @@ public class FlowerPatchFabricClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		for (RegistryObject<Block> registryObject : PatchRegistry.BLOCKS.getEntries()) {
-			if (registryObject.get() instanceof FlowerPatchBlock flowerPatchBlock) {
-				BlockRenderLayerMap.INSTANCE.putBlock(flowerPatchBlock, RenderType.cutout());
+			if (registryObject.get() instanceof PatchBlock) {
+				BlockRenderLayerMap.INSTANCE.putBlock(registryObject.get(), RenderType.cutout());
 			}
 		}
 	}
